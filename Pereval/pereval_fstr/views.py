5cd23deb796ca6787f1b20cc092a1b52ae62c5ec
aliensen36 <142628769+aliensen36@users.noreply.github.com>
2024-03-25 15:01:42 +0300

    Description of views

--- Pereval/pereval_fstr/views.py
+++ Pereval/pereval_fstr/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+import django_filters
+from rest_framework import mixins, response, viewsets
 
-# Create your views here.
+from .models import Pereval
+from .serializers import PerevalSerializer
+
+
+class PerevalViewSet(mixins.CreateModelMixin,
+                     mixins.RetrieveModelMixin,
+                     mixins.UpdateModelMixin,
+                     mixins.ListModelMixin,
+                     viewsets.GenericViewSet):
+    queryset = Pereval.objects.all()
+    serializer_class = PerevalSerializer
+    filter_backends = [django_filters.rest_framework.DjangoFilterBackend]
+    filterset_fields = ['user__email']
+
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+        if instance.status != 'new':
+            return response.Response({'state': '0',
+                                      'message': 'Status not new'})
+        serializer = self.get_serializer(instance,
+                                         data=request.data,
+                                         partial=partial)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+
+        if getattr(instance, '_prefetched_objects_cache', None):
+            instance._prefetched_objects_cache = {}
+
+        return response.Response({'state': '1',
+                                  'message': 'Success'})
