d75fb719d24d94272100ade4f3cc6633793d0a38
aliensen36 <142628769+aliensen36@users.noreply.github.com>
2024-03-25 15:01:09 +0300

    Description of apps

--- Pereval/pereval_fstr/apps.py
+++ Pereval/pereval_fstr/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class PerevalFstrConfig(AppConfig):
+class PerevalConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'pereval_fstr'
+    name = 'pereval'
